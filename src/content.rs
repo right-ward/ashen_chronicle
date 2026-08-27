c75e38ce4609f541a029127531f09b610d215847
rightward <ninja.notleft@proton.me>
2026-08-27 22:02:38 +0330

    feat: register content diagnostics module

--- src/content.rs
+++ src/content.rs
@@ -1,6 +1,8 @@
 pub mod definitions;
+pub mod diagnostics;
 pub mod loader;
 pub mod seeding;
 
 pub use definitions::*;
+pub use diagnostics::campaign_content_load_diagnostics;
 pub use loader::load_campaign_content;
