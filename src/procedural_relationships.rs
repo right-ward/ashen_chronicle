c88ac6237aeb3956d3876753ea01212d35db0c66
rightward <ninja.notleft@proton.me>
2026-09-07 20:09:57 +0330

    fix: remove hidden opportunity generation from relationships

--- src/procedural_relationships.rs
+++ src/procedural_relationships.rs
@@ -93,7 +93,6 @@ pub fn populate_generated_relationships(state: &mut GameState) -> usize {
         }
     }
 
-    crate::procedural_opportunities::populate_generated_opportunities(state);
     added
 }
 
