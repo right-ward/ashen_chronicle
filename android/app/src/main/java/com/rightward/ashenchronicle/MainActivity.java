752e41af723d35932d1e89caf324584e1c43751f
rightward <ninja.notleft@proton.me>
2026-09-15 13:09:14 +0330

    style(android): clean SAF activity imports

--- android/app/src/main/java/com/rightward/ashenchronicle/MainActivity.java
+++ android/app/src/main/java/com/rightward/ashenchronicle/MainActivity.java
@@ -2,7 +2,6 @@ package com.rightward.ashenchronicle;
 
 import android.app.AlertDialog;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
